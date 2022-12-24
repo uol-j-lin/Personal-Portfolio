07636c229b9812081297b1e26faad40c9764e269
Jonathan <114364831+jjlin526@users.noreply.github.com>
2022-12-24 15:03:56 -0500

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+const Footer = (props) => {
+  const bgStyle = { backgroundColor: "#f5f5f5" };
+
+  return (
+    <footer style={bgStyle} className="mt-auto py-5 text-center ">
+      <Container>
+        {props.children}
+        <i className="fas fa-code" /> with <i className="fas fa-heart" /> by{" "}
+        <a
+          rel="noopener"
+          href="https://github.com/jjlin526"
+          aria-label="My GitHub"
+        > <span className="badge bg-dark">
+            Jonathan Lin
+          </span>
+        </a>{" "}
+        using <i className="fab fa-react" />
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/editable-stuff/config.js
@@ -0,0 +1,171 @@
+// Navigation Bar SECTION
+const navBar = {
+  show: true,
+};
+
+// Main Body SECTION
+const mainBody = {
+  gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
+  firstName: "Jonathan",
+  middleName: "",
+  lastName: "Lin",
+  message:
+    " Passionate about changing the world with innovative solutions in technology ",
+  icons: [
+    {
+      image: "fa-github",
+      url: "https://github.com/jjlin526",
+    },
+    {
+      image: "fa-facebook",
+      url: "https://www.facebook.com/",
+    },
+    {
+      image: "fa-instagram",
+      url: "https://www.instagram.com/",
+    },
+    {
+      image: "fa-linkedin",
+      url: "https://www.linkedin.com/in/jonathan-l526/",
+    },
+    {
+      image: "fa-twitter",
+      url: "https://www.twitter.com/",
+    },
+  ],
+};
+
+// ABOUT SECTION
+// If you want the About Section to show a profile picture you can fill the profilePictureLink either with:
+//a) your Instagram username
+//      i.e:profilePictureLink:"johnDoe123",
+//b) a link to an hosted image
+//      i.e:profilePictureLink:"www.picturesonline.com/johnDoeFancyAvatar.jpg",
+//c) image in "editable-stuff" directory and use require("") to import here,
+//      i.e: profilePictureLink: require("../editable-stuff/jonathanlin.png"),
+//d) If you do not want any picture to be displayed, just leave it empty :)
+//      i.e: profilePictureLink: "",
+// For Resume either provide link to your resume or import from "editable-stuff" directory
+//     i.e resume: require("../editable-stuff/resume.pdf"),
+//         resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+
+const about = {
+  show: true,
+  heading: "About Me",
+  imageLink: require("../editable-stuff/jonathanlin.png"),
+  imageSize: 375,
+  message:
+    "My name is Jonathan Lin. I am a Software Engineering Intern at Rio Tinto in Montreal QC, and am pursuing a Bachelor of Computer Science at the University of London. I am passionate about Full Stack Development, Data Science, Machine Learning and Artificial Intelligence. I’m currently building microservices using Spring Boot and automating workflows using AWS Lambda. I’m learning Kubernetes, C# and ASP.NET, Tensorflow, Keras and Pytorch.",
+  resume:
+    require("../editable-stuff/resume.pdf"),
+};
+
+// PROJECTS SECTION
+// Setting up project length will automatically fetch your that number of recently updated projects, or you can set this field 0 to show none.
+//      i.e: reposLength: 0,
+// If you want to display specfic projects, add the repository names,
+//      i.e ["repository-1", "repo-2"]
+const repos = {
+  show: true,
+  heading: "Recent Projects on Github",
+  gitHubUsername: "jjlin526",
+  reposLength: 0,
+  specificRepos: [
+    "Dual-Deck-DJ-Application",
+    "Twitter-NLTK-ML-Sentiment-Analysis",
+    "Full-Stack-MERN-Fitness-Tracker-Web-Application",
+    "Full-Stack-MERN-Project-Management-Application",
+  ],
+};
+
+// Leadership SECTION
+const leadership = {
+  show: false,
+  heading: "Leadership",
+  message:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
+  images: [
+    {
+      img: require("../editable-stuff/jonathanlin.png"),
+      label: "First slide label",
+      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+    },
+    {
+      img: require("../editable-stuff/jonathanlin.png"),
+      label: "Second slide label",
+      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+    },
+  ],
+  imageSize: {
+    width: "615",
+    height: "450",
+  },
+};
+
+// SKILLS SECTION
+const skills = {
+  show: true,
+  heading: "Skill Set",
+  programmingLanguages: [
+    { name: "Python", value: 75 },
+    { name: "SQL", value: 70 },
+    { name: "JavaScript", value: 90 },
+    { name: "C++", value: 95 },
+    { name: "TypeScript", value: 70 },
+    { name: "Go", value: 70 },
+    { name: "HTML/CSS", value: 90 },
+    { name: "Java", value: 90 },
+  ],
+  toolsAndFrameworks: [
+    { name: "Node.js", value: 95 },
+    { name: "Express", value: 85 },
+    { name: "React", value: 80 },
+    { name: "MongoDB", value: 80 },
+    { name: "Docker", value: 80 },
+    { name: "Git/Github", value: 90 },
+    { name: "GraphQL", value: 85 },
+    { name: "Apollo", value: 80 },
+  ],
+};
+
+// GET IN TOUCH SECTION
+const getInTouch = {
+  show: true,
+  heading: "Get In Touch",
+  message:
+    "I am currently looking for Summer '23 Software Engineering Internship opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
+  email: "uoljlin@gmail.com",
+};
+
+const experiences = {
+  show: false,
+  heading: "Experiences",
+  data: [
+    {
+      role: "Software Engineer", // Here Add Company Name
+      companylogo: require("../assets/img/dell.png"),
+      date: "June 2018 – Present",
+    },
+    {
+      role: "Front-End Developer",
+      companylogo: require("../assets/img/boeing.png"),
+      date: "May 2017 – May 2018",
+    },
+  ],
+};
+
+// Blog SECTION
+// const blog = {
+//   show: false,
+// };
+
+export {
+  navBar,
+  mainBody,
+  about,
+  repos,
+  skills,
+  leadership,
+  getInTouch,
+  experiences,
+};
